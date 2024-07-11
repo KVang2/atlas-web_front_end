9278e29566e98f55748b9f2702eca351b376ccb7
kvang2 <kevinvaj88@hotmail.com>
2024-07-10 23:41:27 -0500

    update tasks

---
+++ Javascript_advanced/10-prime.js
@@ -0,0 +1,28 @@
+function countPrimeNumbers() {
+    let primeCount = 0;
+  
+    for (let n = 2; n <= 100; n++) {
+      let isPrime = true;
+  
+      for (let i = 2; i < n; i++) {
+        if (n % i === 0) {
+          isPrime = false;
+          break;
+        }
+      }
+      if (isPrime) {
+          primeCount++;
+      }
+    }
+  
+    return primeCount;
+  }
+  
+  let start = performance.now();
+  
+  for (let i = 0; i < 100; i++) {
+    countPrimeNumbers();
+  }
+  let end = performance.now();
+  
+  console.log('execution time of printing countPrimeNumbers was ${end - start} milliseconds.');
