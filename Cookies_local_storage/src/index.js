f01528155eb0df57e08eda1031e3c127bf7e2ac8
kvang2 <kevinvaj88@hotmail.com>
2024-07-17 22:33:24 -0500

    update changes

--- Cookies_local_storage/src/index.js
+++ Cookies_local_storage/src/index.js
@@ -1,14 +1,17 @@
 // Javascript function to set cookies
 function setCookies() {
+    // Get values
     const firstname = document.getElementById('firstname').value;
     const email = document.getElementById('email').value;
 
+    // set cookies with obtained values
     document.cookie = `firstname=${firstname}; path=/`;
     document.cookie = `email=${email}; path=/`; 
 }
 
 // Function to show cookies
 function showCookies() {
+    // paragraph element to show cookies
     const p = document.createElement('p');
     const cookies = document.cookie;
 
